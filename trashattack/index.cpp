be2203982c85ade081942f29b8be11bdd3722b3b
ЯpЛ <zennloveit@gmail.com>
2025-10-17 14:04:59 +0700

    Create index.cpp

---
+++ trashattack/index.cpp
@@ -0,0 +1,10 @@
+<div class="box" data-aos="fade-up" data-aos-duration="700" data-aos-easing="ease-out-cubic">
+          <div class="box-left">
+            <div class="box-title">GithubSearch</div>
+            <div class="box-sub">GithubSearch</div>
+          </div>
+          <div class="box-right" role="group" aria-label="/api actions">
+            <div class="api-path">/api/github</div>
+            <button class="btn-get" data-href="/api/github/?search=" title="GET /api/github">GET</button>
+          </div>
+        </div>
